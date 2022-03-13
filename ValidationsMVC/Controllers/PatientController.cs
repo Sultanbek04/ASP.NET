828861e2df9de5c0577e3a10923d33770625feee
Sultanbek <msultanbek@mail.ru>
2022-03-13 13:52:24 +0400

    H

---
+++ ValidationsMVC/Controllers/PatientController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValidationsMVC.Models;
+
+namespace ValidationsMVC.Controllers
+{
+  public class PatientController : Controller
+  {
+    // GET: Patient
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Patient patient)
+    {
+      return View();
+    }
+
+    public ActionResult Edit()
+    {
+      return View();
+    }
+
+    public ActionResult CheckBalanceOfThePatient(decimal accountBalance)
+    {
+      if (accountBalance < 100)
+      {
+        return Json(false, JsonRequestBehavior.AllowGet);
+      }
+      return Json(true, JsonRequestBehavior.AllowGet);
+    }
+
+    public ActionResult IsAcceptableInsurance(string nameOfInsurance)
+    {
+      string[] acceptableInsurances = new string[]
+      {
+        "Sultanbek is The Best",
+        "Sultanbek is The Smartest",
+        "Sultanbek is a genius"
+      };
+
+      for (int i = 0; i < acceptableInsurances.Length; ++i)
+      {
+        if (nameOfInsurance == acceptableInsurances[i])
+        {
+          return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
+      }
+      return Json(false, JsonRequestBehavior.AllowGet);
+    }
+  }
+}
---
+++ ValidationsMVC/Models/Patient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ValidationsMVC.Models
+{
+  [Table("Patient")]
+  public class Patient
+  {
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "First name is mandatory")]
+    [StringLength(100)]
+    public string FirstName { get; set; }
+    [Required(ErrorMessage = "Last name is necessary")]
+    [StringLength(100)]
+    public string LastName { get; set; }
+    [Required(ErrorMessage = "Age is obligatory")]
+    [Range(0, 300)]
+    public int Age { get; set; }
+    [Required(ErrorMessage = "Please choose a gender")]
+    public bool IsMan { get; set; }
+    [Required(ErrorMessage = "Please let us know is patient ill")]
+    public bool IsIll { get; set; }
+
+    public string TypeOfDisease { get; set; }
+    [Remote(action: "CheckBalanceOfThePatient", controller: "Patient", ErrorMessage = "Sorry minimum required balance is 100")]
+    public decimal AccountBalance { get; set; }
+
+    [Remote(action: "IsAcceptableInsurance", controller: "Patient", ErrorMessage ="We don't accept your Insurance")]
+    public string NameOfInsurance { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; }
+
+
+  
+  }
+}
