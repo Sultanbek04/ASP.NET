ca0847087c60eb152abb28d645c4c27b7da127b8
Sultanbek <msultanbek@mail.ru>
2022-03-13 17:11:02 +0400

    First commit

---
+++ MVCReturningDataAsJSONAndString/Context/DB.cs
@@ -0,0 +1,17 @@
+﻿using MVCReturningDataAsJSONAndString.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCReturningDataAsJSONAndString.Context
+{
+  public class DB : DbContext
+  {
+    public DB() : base("name=connStr") { }
+
+    public DbSet<City> Student { get; set; }
+
+  }
+}
---
+++ MVCReturningDataAsJSONAndString/Controllers/JSONAndStringController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCReturningDataAsJSONAndString.Context;
+using MVCReturningDataAsJSONAndString.Models;
+
+namespace MVCReturningDataAsJSONAndString.Controllers
+{
+  public class JSONAndStringController : Controller
+  {
+    // GET: JSONAndString
+    private DB db = new DB();
+
+    // GET: Student
+    public JsonResult Index()
+    {
+      return Json(db.Student.ToList(), JsonRequestBehavior.AllowGet);
+    }
+
+    public string GetAreaFormulaOfCircle()
+    {
+      return "π*r^2";
+    }
+
+  }
+}
