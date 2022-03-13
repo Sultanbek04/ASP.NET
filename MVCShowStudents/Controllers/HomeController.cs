1618c42bfc41477d650ed9ad9e921b4d15bbaac9
Sultanbek <msultanbek@mail.ru>
2022-03-13 17:10:56 +0400

    First commit

---
+++ MVCShowStudents/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCShowStudents.Models;
+
+namespace MVCShowStudents.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult ShowStundets()
+    {
+      CultureInfo provider = CultureInfo.InvariantCulture;
+      List<Student> list = new List<Student>()
+      {
+        new Student{ Id=1, LastName="Myrzakhmet", Birthday=DateTime.Parse("04/26/2004") },
+        new Student {Id=2, LastName="Nurimov", Birthday=DateTime.Parse("10/21/2001")},
+        new Student {Id=3, LastName="Baibatyrov", Birthday=DateTime.Parse("10/21/1976")}
+
+    };
+      return View(list);
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+  }
+}
---
+++ MVCShowStudents/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCShowStudents.Models
+{
+  public class Student
+  {
+    public int Id { get; set; }
+    public string LastName { get; set; }
+    public DateTime Birthday { get; set; }
+  }
+}
