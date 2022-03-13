7dfd07daca93b90df1d4e92e9f02f9d8b8e7a71a
Sultanbek <msultanbek@mail.ru>
2022-03-13 17:11:14 +0400

    First commit

---
+++ MVCVBVDShowCountries/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCVBVDShowCountries.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult ShowCountries()
+    {
+      List<Country> countries=new List<Country>()
+      {
+        new Country { Id=1, Name="Kazakhstan"},
+        new Country {Id=2, Name="Russia" },
+        new Country {Id=3, Name="USA" },
+      };
+
+      ViewBag.Countries=countries;
+      ViewData["country"]=countries;
+      return View(countries);
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+  }
+}
