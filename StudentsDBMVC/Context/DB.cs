5505139fa537ab79b5ece2c03cf7887df48778fb
Sultanbek <msultanbek@mail.ru>
2022-03-13 17:11:24 +0400

    First commit

---
+++ StudentsDBMVC/Context/DB.cs
@@ -0,0 +1,17 @@
+﻿using StudentsDBMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StudentsDBMVC.Context
+{
+  public class DB : DbContext
+  {
+    public DB() : base("name=connStr") { }
+
+    public DbSet<Student> Student { get; set; }
+
+  }
+}
